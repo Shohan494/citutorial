789818b6ed806295c8ede305d242fcd269488100
shohan494 <shohan12224943@gmail.com>
2017-11-05 07:45:01 +0600

    news model and controller with views

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,3 +58,6 @@ $route['translate_uri_dashes'] = FALSE;
 
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
